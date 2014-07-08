5380235b747c523b719cf52d7fefdfb158a9bebb
Adam Vesecký <vesecky.adam@gmail.com>
2014-07-08 22:25:51 +0200

    Design

--- Development/WPFSolution/CopterDown/CopterDown/GameLoop.cs
+++ Development/WPFSolution/CopterDown/CopterDown/GameLoop.cs
@@ -5,6 +5,11 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using CopterDown.Behavior;
+using CopterDown.Core;
 
 namespace CopterDown
 {
@@ -15,12 +20,22 @@ namespace CopterDown
         private Timer _drawTimer;
         private Timer _updateTimer;
         private Canvas _canvas;
+        private GameObject root;
+        private Dispatcher _disp;
 
-        public GameLoop(Canvas canvas, int drawInterval, int updateInterval)
+        public GameLoop(Dispatcher disp, Canvas canvas, int drawInterval, int updateInterval)
         {
             this._drawInterval = drawInterval;
             this._updateInterval = updateInterval;
             this._canvas = canvas;
+            this.root = new GameObject(null);
+            this._disp = disp;
+
+            Rectangle rect = new Rectangle();
+            rect.Fill = Brushes.Black;
+            rect.Width = 100;
+            rect.Height = 200;
+            root.Behaviors.Add(new RenderBehavior(rect));
         }
 
         public void Start()
@@ -33,7 +48,12 @@ namespace CopterDown
 
         private void Draw(object state)
         {
-            
+            _disp.Invoke(() =>
+            {
+
+                _canvas.Children.Clear();
+                root.OnMessage(new Message(MessageType.RENDER, _canvas));
+            });
         }
 
         private void Update(object state)
