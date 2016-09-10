e0cbc4bbc23b5c0434b44e58436ce08aa85ad233
Adam Vesecký <vesecky.adam@gmail.com>
2016-09-10 09:50:51 +0200

    Fixed lua source code for compilation for android Date: March 17, 2016 19:52:28

--- COGengine/Source/Tests/LuaTest.h
+++ COGengine/Source/Tests/LuaTest.h
@@ -35,15 +35,12 @@ int LuaGlobalFunction(lua_State *L)
 TEST_CASE("Lua tests", "[class]")
 {
 
-	SECTION("Simple lua function call")
+	SECTION("Simple lua Lua-C++ function call")
 	{
 		// create lua state
-		lua_State* pl = lua_open();
+		lua_State* pl = luaL_newstate();
 		// enable access to standard libs
-		luaopen_base(pl);
-		luaopen_table(pl);
-		luaopen_string(pl);
-		luaopen_math(pl);
+		luaL_openlibs(pl);
 
 
 		lua_register(pl, "LuaGlobalFunction", LuaGlobalFunction);
@@ -70,6 +67,81 @@ param = LuaGlobalFunction(param)"
 		
 		REQUIRE(param == 123);
 		REQUIRE(lastGlobalFuncParam == 1234);
+
+		lua_pop(pl, 1);
+		lua_close(pl);
+	}
+
+	SECTION("Simple lua C++-Lua function call")
+	{
+		lua_State* pl = luaL_newstate();
+		luaL_openlibs(pl);
+
+#define lua_sample " \
+		function add(a, b) \
+			return (a + b); \
+		end; "
+		
+		int status = luaL_loadstring(pl, lua_sample);
+		if (status != 0) {
+			std::cerr << "-- " << lua_tostring(pl, -1) << std::endl;
+		}
+		else {
+			// execute program
+			lua_pcall(pl, 0, LUA_MULTRET, 0);
+		}
+
+		lua_getglobal(pl, "add");
+		lua_pushnumber(pl, 10);
+		lua_pushnumber(pl, 20);
+
+		// 2 input parameters and 1 return parameter
+		lua_call(pl, 2, 1);
+
+		int result = (int)lua_tonumber(pl, -1);
+
+		REQUIRE(result == 30);
+
+		lua_pop(pl, 1);
+		lua_close(pl);
+	}
+
+	SECTION("Lua classes")
+	{
+		lua_State* pl = luaL_newstate();
+		luaL_openlibs(pl);
+
+#define lua_sample " \
+class 'Person' \
+function Person:__init(salary) \
+  self.Salary = salary \
+end \
+\
+function Person:GetSalary()\
+  return self.Salary \
+end \
+\
+thomas = Person(12000) "
+
+		int status = luaL_loadstring(pl, lua_sample);
+		if (status != 0) {
+			std::cerr << "-- " << lua_tostring(pl, -1) << std::endl;
+		}
+		else {
+			// execute program
+			lua_pcall(pl, 0, LUA_MULTRET, 0);
+		}
+
+		lua_getglobal(pl, "add");
+		lua_pushnumber(pl, 10);
+		lua_pushnumber(pl, 20);
+
+		// 2 input parameters and 1 return parameter
+		lua_call(pl, 2, 1);
+
+		int result = (int)lua_tonumber(pl, -1);
+
+		REQUIRE(result == 30);
 
 		lua_pop(pl, 1);
 		lua_close(pl);
