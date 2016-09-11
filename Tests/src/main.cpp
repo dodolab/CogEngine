48e872b8d191202a6f6ae93d5b7ec8f761972d6d
Adam Vesecký <vesecky.adam@gmail.com>
2016-09-11 21:48:34 +0200

    Add project for testing

---
+++ Tests/src/main.cpp
@@ -0,0 +1,27 @@
+#define CATCH_CONFIG_RUNNER
+
+// tests can run only in debug mode
+#ifdef _DEBUG
+
+#include "FlagsTest.h"
+#include "TransformTest.h"
+#include "SQLTest.h"
+#include "MeasureTest.h"
+#include "NetworkTest.h"
+#include "StateMachineTest.h"
+#include "GoalTest.h"
+#include "SettingsTest.h"
+#include "EngineTest.h"
+#include "StrIdTest.h"
+#include "LuaTest.h"
+#include "MonteCarloTest.h"	
+#include "CoroutineTest.h"
+#include "MathTest.h"
+
+int main() {
+	ofSetupOpenGL(800, 450, OF_WINDOW);
+	int result = Catch::Session().run();
+	return result;
+}
+
+#endif
